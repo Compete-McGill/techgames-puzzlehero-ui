b16d6b66b59a5a830437f3dbe3a4c22bf09209b9
Imad Dodin <imad.dodin@mail.mcgill.ca>
2021-02-19 16:07:01 -0500

    Update Event Description on Home Page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -5,16 +5,30 @@
       <v-container>
         <v-row>
           <v-card-title>Welcome to Tech Games!</v-card-title>
-          <v-card-text class="grey--text">Fri, Feb 19 2020</v-card-text>
+          <v-card-text class="grey--text">Sat, Feb 20 2021</v-card-text>
+          <v-card-text>
+            Tech Games is McGill's Olympics of practical Software Engineering
+            and Computer Science challenges. Grab some friends, your favorite
+            (caffeinated) drink and your IDE of choice and put your skills to
+            the test from the comfort of your own homes for the chance for some
+            cool prizes.
+            <br />
+            This year, join us on Gather Town: team up in groups of 3 to 5, rack
+            up points by competing in challenges in Backend Development, Mobile
+            Development, Functional Programming, Relay Programming, and Xtreme
+            Theory (that's right, not just regular theory 😉) challenges, and
+            flex your wits by grinding through waves of puzzles!
+          </v-card-text>
 
           <v-card-title>HOW TO GET STARTED</v-card-title>
           <v-card-text>
-            1. Make an account by registering with your <b>McGill email</b>. Use
-            the register button in the top right.
+            1. Make an account by registering with your <b>McGill email</b>.
+            (Use the register button in the top right.)
             <br />
             2. If one of your teammates has already made a team, ask the creator
             of your team to add you to it in the "Team" tab! Otherwise, create a
             team in the "Team" tab.
+            <i>There should be only one team created per group!</i>
             <br />
             3. All login info for Gather Town should be included in your
             registration confirmation email!
