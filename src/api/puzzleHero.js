fabb0d7f0b78902023362479ca9e1c05e6c0fccc
Imad Dodin <imad.dodin@mail.mcgill.ca>
2021-02-19 17:39:42 -0500

    Hardcode Backend Service IP

--- src/api/puzzleHero.js
+++ src/api/puzzleHero.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const url = process.env.VUE_APP_PUZZLEHERO_API_URL || "http://localhost:3000";
+const url = process.env.VUE_APP_PUZZLEHERO_API_URL || "http://34.95.25.177/";
 export const puzzleHeroApi = axios.create({
   baseURL: url
 });
