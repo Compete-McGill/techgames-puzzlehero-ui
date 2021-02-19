4fb97b42dee69ee8ff3e159459a9421895d5e954
Valerian Clerc <valerian.clerc@gmail.com>
2021-02-19 11:29:48 -0500

    Init nicer home page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,56 @@
+<!-- TODO: implement Announcement card for Home view -->
 <template>
-  <div class="home">
-    <h1>Home!</h1>
-    <p>
-      This is where some announcement info could go, or instructions on Tech
-      Games events
-    </p>
-    <!-- <p>User: {{ this.$store.state.snackbarInfo }}</p> -->
-  </div>
+  <v-container>
+    <v-card class="pa-2">
+      <v-container>
+        <v-row>
+          <v-card-title>Welcome to Tech Games!</v-card-title>
+          <v-card-text class="grey--text">Fri, Feb 19 2020</v-card-text>
+
+          <v-card-title>HOW TO GET STARTED</v-card-title>
+          <v-card-text>
+            1. Make an account by registering with your <b>McGill email</b>. Use
+            the register button in the top right.
+            <br />
+            2. If one of your teammates has already made a team, ask the creator
+            of your team to add you to it in the "Team" tab! Otherwise, create a
+            team in the "Team" tab.
+            <br />
+            3. All login info for Gather Town should be included in your
+            registration confirmation email!
+          </v-card-text>
+
+          <v-card-text><h3>Schedule</h3></v-card-text>
+          <v-card-text>TODO: add schedule image here</v-card-text>
+          <v-img>
+            <!-- TODO: Schedule image -->
+          </v-img>
+        </v-row>
+      </v-container>
+    </v-card>
+  </v-container>
 </template>
 
 <script>
 export default {
   name: "Home",
-  components: {}
+  methods: {
+    openLink(input) {
+      switch (input) {
+        case "discord":
+          window.open("https://discord.gg/JhZQWPW", "_blank");
+          break;
+        default:
+          break;
+      }
+    }
+  }
 };
 </script>
+
+<style scoped>
+a {
+  text-decoration: none;
+  font-weight: bold;
+}
+</style>
